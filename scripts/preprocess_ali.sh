1551b9d3f52eac96bdc73e33b1d21400c1596c67
YUE XIANGHU <yuexianghubit@gmail.com>
2025-01-21 19:02:56 +0800

    Update preprocess_ali.sh

--- scripts/preprocess_ali.sh
+++ scripts/preprocess_ali.sh
@@ -1,11 +1,11 @@
 data_path=/mntcephfs/lab_data/zxy/Alimeeting
 
 echo " Process dataset: Train/Eval dataset, get json files"
-python prepare_data.py \
+python scripts/prepare_data.py \
     --data_path ${data_path} \
     --type Eval \
 
-python prepare_data.py \
+python scripts/prepare_data.py \
     --data_path ${data_path} \
     --type Train \
 
